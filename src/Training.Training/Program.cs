c895f012730dc26bf3908c63cafcbc191bae2225
Olwynion <mrfanta2003@gmail.com>
2026-06-04 10:22:41 +0300

    HTTP/GRPC порты вынесены в конфигурацию

--- src/Training.Training/Program.cs
+++ src/Training.Training/Program.cs
@@ -7,13 +7,16 @@ AppContext.SetSwitch("Microsoft.AspNetCore.Server.Kestrel.Experimental.DisableHt
 
 builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 
+var httpPort = builder.Configuration.GetValue("HttpPort", 5006);
+var grpcPort = builder.Configuration.GetValue("GrpcPort", 5003);
+
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5000, listenOptions =>
+    options.ListenAnyIP(httpPort, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1;
     });
-    options.ListenAnyIP(5003, listenOptions =>
+    options.ListenAnyIP(grpcPort, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http2;
     });
